2faf1c4efe95c67aa253f7fded0e6ddfe8527e78
Azhar Parvaiz <37549602+azharparvaiz@users.noreply.github.com>
2025-03-10 10:51:50 +0530

    Declared variable "i" inside the loop

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Getting elements with class="myColumn"
 let elements = document.getElementsByClassName("myColumn");
 
-// A variable for looping
-let i;
-
 // Function for Viewing Images as List
 function list() {
-    for (i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
     elements[i].style.flex = "100%";
   }
 }
